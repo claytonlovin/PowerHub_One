7770d3be0865a00bd71067c65652be17fe1fe41f
Clayton Silva <57988870+claytonlovin@users.noreply.github.com>
2022-12-28 20:08:24 -0300

    finalizing project

--- setting/config.py
+++ setting/config.py
@@ -12,7 +12,7 @@ app.secret_key = 'chave secreta'
 # CONECTANDO AO BANCO
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'clayton'
-app.config['MYSQL_PASSWORD'] = '0209012'
+app.config['MYSQL_PASSWORD'] = '****'
 app.config['MYSQL_DB'] = 'DB_POWERHUB_HM'
 
 # INICIALIZANDO O MYSQL
