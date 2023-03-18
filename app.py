89bd09a1a6f3f1075997bd2b98e55422ea6c6d60
cs0209012 <luvinstart@gmail.com>
2023-03-17 21:50:07 -0300

    alter string path

--- app.py
+++ app.py
@@ -3,8 +3,11 @@
 #from traceback import print_tb
 from setting.config import *
 from router.router import *
+import os
 
+'''if __name__ == '__main__':
+    app.run(debug=True)
+'''
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
